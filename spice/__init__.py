d99b601147c08d0f7db68f31d49ca955b855f1a7
Christina Galani <>
2021-03-26 16:24:03 +0100

    initial commit

---
+++ spice/__init__.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'db792362d1434963fb710db181132774'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+#this was not necessary to display the username
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
+
+from spice import routes
