7028f4d4b60ac159e9fa1410979a322991dc5574
Christina Galani <>
2021-03-29 13:16:18 +0200

    added old school react

--- spice/__init__.py
+++ spice/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'db792362d1434963fb710db181132774'
